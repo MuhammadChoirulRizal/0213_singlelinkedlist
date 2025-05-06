a618721b95795b623836fae9cd83acb5fa7deacf
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-05-06 12:30:02 +0700

    membuat traverse

--- single_linkedlist.cpp
+++ single_linkedlist.cpp
@@ -63,26 +63,22 @@ void addNode()
         return (*current != NULL);
     }
     bool delNode(int nim)
-    
+    {
+        node *current, *previous;
+        if (search(nim, &previous, &current))
+            return false;
+
+        if (current == START)
+            START == START->next;
+        else
+            previous->next = current->next;
+        delete current;
+        return;
+    }
     void traverse()
     {
-        if (listEmpty())
-        {
-            cout << "\nList kosong\n";
-        }
-        else
-        {
-            cout << "\nData didalam list:\n";
-            node *currentNode = START;
-            while (currentNode != NULL)
-            {
-                cout << currentNode->noMhs << endl;
-                currentNode = currentNode->next;
-            }
-            cout << endl;
-        }
-    }
-}
+    
+};
 
 int main()
 {
